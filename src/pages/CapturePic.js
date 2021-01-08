57519ba53d6ce1ac57d875a0602edec159736a6c
Apichai Pinyorattanayotee <winterapc@gmail.com>
2021-01-08 23:19:35 +0700

    finish detect

--- src/pages/CapturePic.js
+++ src/pages/CapturePic.js
@@ -1,31 +1,35 @@
-import axios from 'axios';
+// import axios from 'axios';
 import React, { useCallback, useRef, useState } from 'react'
 import Webcam from 'react-webcam';
 import styled from 'styled-components';
+const nvision = require("@nipacloud/nvision/dist/browser/nvision.js");
 
-const api = 'https://nvision.nipa.cloud/api/v1/cdb29f355cb4059995e054208f8cc73c327e9bbc3a0c290e7d88c58022f3e4f8a6c491cf8411c1b1291068c25c15042aac'
+const api = 'cdb29f355cb4059995e054208f8cc73c327e9bbc3a0c290e7d88c58022f3e4f8a6c491cf8411c1b1291068c25c15042aac'
 
 const CapturePic = () => {
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null)
+  const [getData, setgetData] = useState('');
 
   const capture = useCallback(() => {
     const imgeSrc = webcamRef.current.getScreenshot({width: 1200, height: 1000});
     setImgSrc(imgeSrc)
   }, [webcamRef, setImgSrc])
-
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const formData = new FormData()
-    await axios.post(api, formData, imgSrc, {
-      rawData: imgSrc
-    })
-    .then(response => {
-      alert('upload success')
-      console.log(response);
-    }).catch(err => {
-      console.log(err, 'err message')
-    })
+    const str = imgSrc.indexOf(',')
+    const convert = imgSrc.slice(str+1)
+    const objectDetectionService = nvision.objectDetection({
+      apiKey: api
+    });
+    objectDetectionService.predict({
+      rawData: convert
+    }).then((result) => { 
+      const toStr = JSON.stringify(result)
+      setgetData(toStr)
+      console.log(result);
+    });
   }
 
   return (
@@ -41,9 +45,11 @@ const CapturePic = () => {
         {imgSrc && (
           <img
             alt={imgSrc}
+            src={imgSrc}
           />
         )}
-        <Button>Upload Pic</Button>
+        <Button>Send To API</Button>
+        <p>{getData}</p>
       </form>
     </Warpper>
   )
